5e7f7c7e740c9a36c0622f7c4e9adb02298f909d
yeahieakhan <yeahieakhan2017@gmail.com>
2026-02-15 16:16:33 +0600

    11-2 Working with Environment-Based Configuration done

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({path : path.join(process.cwd(),".evn")})
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 5000,
+};
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
 
 // create server
 const server: Server = http.createServer(
@@ -18,6 +19,6 @@ const server: Server = http.createServer(
   }
 );
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+server.listen(config.port, () => {
+  console.log(`Server running on port :  ${config.port}`);
 });
